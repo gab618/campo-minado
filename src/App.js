477fc8740afa29ee3b315a83e97e8ed5f3c8a015
Gabriel <knuckles4x@gmail.com>
2020-11-05 20:32:12 -0300

    minesweeper game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Minesweeper from 'react-minesweeper';
 
 function App() {
+  const [minesweeperKey, setMinesweeperKey] = useState(0);
+
+  const restartMinesweeper = () => {
+    setMinesweeperKey(minesweeperKey + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="minesweeper__header">
+        <button className="minesweeper__restart" onClick={restartMinesweeper}>
+          😂
+        </button>
+      </div>
+      <Minesweeper
+        key={minesweeperKey}
+        onWin={() => console.log('GAME WON')}
+        onLose={() => console.log('GAME LOST')}
+        bombChance={0.15} // 15% chance that a field will contain a bomb
+        width={10} // amount of fields horizontally
+        height={10} // amount of fields vertically
+      />
     </div>
   );
 }
