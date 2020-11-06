93489dd1a1323553ed4de2abc612433d37cee892
Gabriel <knuckles4x@gmail.com>
2020-11-06 10:05:30 -0300

    submit points

--- src/App.js
+++ src/App.js
@@ -1,29 +1,69 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Minesweeper from 'react-minesweeper';
+import api from './services/api';
 
 function App() {
   const [minesweeperKey, setMinesweeperKey] = useState(0);
+  const [username, setUsername] = useState('');
+  const [key, setKey] = useState('');
+
+  useEffect(() => {
+    getKey();
+  }, []);
+
+  async function getKey() {
+    const response = await api.get('api/game');
+    setKey(response.data.key);
+  }
+
+  async function submitPoints() {
+    const hash = 'U2FsdGVkX1+TUM9/CfwvfxxYHxc48j0WLYwrsWotnRA=';
+    await api.post('api/game', {
+      key,
+      hash,
+      user: username,
+    });
+    alert(`Parebéns! +30 pontos para ${username}`);
+  }
 
   const restartMinesweeper = () => {
     setMinesweeperKey(minesweeperKey + 1);
+    getKey();
+  };
+
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
   };
 
   return (
-    <div className="container">
-      <div className="minesweeper__header">
-        <button className="minesweeper__restart" onClick={restartMinesweeper}>
-          😂
-        </button>
+    <>
+      <h1>Campo minado</h1>
+      <a href="https://twitch.tv/ezrealblindado">
+        pontos na live twitch.tv/ezrealblindado
+      </a>
+      <div className="container">
+        <input
+          className="user-input"
+          type="text"
+          placeholder="Seu nick da twitch aqui"
+          onChange={handleChangeUsername}
+          value={username}
+        />
+        <div className="minesweeper__header">
+          <button className="minesweeper__restart" onClick={restartMinesweeper}>
+            🤡
+          </button>
+        </div>
+        <Minesweeper
+          key={minesweeperKey}
+          onWin={submitPoints}
+          onLose={() => alert('Você perdeu!')}
+          bombChance={0.15} // 15% chance that a field will contain a bomb
+          width={10} // amount of fields horizontally
+          height={10} // amount of fields vertically
+        />
       </div>
-      <Minesweeper
-        key={minesweeperKey}
-        onWin={() => console.log('GAME WON')}
-        onLose={() => console.log('GAME LOST')}
-        bombChance={0.15} // 15% chance that a field will contain a bomb
-        width={10} // amount of fields horizontally
-        height={10} // amount of fields vertically
-      />
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'react-minesweeper/lib/minesweeper.css';
+import './styles.css';
 
 import App from './App';
 
